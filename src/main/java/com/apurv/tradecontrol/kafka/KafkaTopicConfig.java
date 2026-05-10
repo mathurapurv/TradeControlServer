f006821e6051faab614753e3bfd5f6f2ad6a32c5
mathurapurv <mathur.apurv@gmail.com>
2026-05-10 23:15:07 +0530

    sync to master - everything
    Signed-off-by: mathurapurv <mathur.apurv@gmail.com>

--- src/main/java/com/apurv/tradecontrol/kafka/KafkaTopicConfig.java
+++ src/main/java/com/apurv/tradecontrol/kafka/KafkaTopicConfig.java
@@ -1,4 +1,35 @@
 package com.apurv.tradecontrol.kafka;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaTopicConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Value(value = "${topic.name.trade-confirmation}")
+    private String tradeConfirmationTopicName;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic trdConfirmationTopic() {
+        return new NewTopic(tradeConfirmationTopicName, 1, (short) 1);
+    }
+
+
 }
--- src/main/java/com/apurv/tradecontrol/notification/dispatcher/TradeConfirmationNotificationDispatcher.java
+++ src/main/java/com/apurv/tradecontrol/notification/dispatcher/TradeConfirmationNotificationDispatcher.java
@@ -1,4 +1,44 @@
 package com.apurv.tradecontrol.notification.dispatcher;
 
+import com.apurv.tradecontrol.entity.Trade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
 public class TradeConfirmationNotificationDispatcher {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value(value = "${topic.name.trade-confirmation}")
+    private String tradeConfirmationTopicName;
+
+    public void dispatch(Trade trade){
+        tradeConfirmationNotification(trade);
+    }
+
+
+    public void  tradeConfirmationNotification(Trade trade) {
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(tradeConfirmationTopicName, trade.getTradeID());
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                System.out.println("Sent message=[" + trade.getTradeID() +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            } else {
+                System.out.println("Unable to send message=[" +
+                        trade.getTradeID() + "] due to : " + ex.getMessage());
+            }
+        });
+    }
+
+
+
+
+
+
 }
